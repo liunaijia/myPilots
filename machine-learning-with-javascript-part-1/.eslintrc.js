597740a922e637d464e16e00b278be3f17584002
Naijia Liu <naijialiu@geniesolutions.com.au>
2017-07-07 12:06:02 +1000

    add eslint

---
+++ machine-learning-with-javascript-part-1/.eslintrc.js
@@ -0,0 +1,17 @@
+// This is shared rules for all environments
+module.exports = {
+  extends: [
+    'airbnb-base',
+  ],
+  // parserOptions: {
+  //   sourceType: 'module'
+  // },
+  // env: {
+  //   jquery: true,
+  //   es6: true,
+  //   commonjs: true,
+  //   browser: true,
+  //   node: true,
+  //   'jest/globals': true
+  // }
+};
