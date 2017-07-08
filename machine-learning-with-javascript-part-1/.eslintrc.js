166b5306f0e65fdbdfcb22b56c378f74b374ba0c
Naijia Liu <naijialiu@geniesolutions.com.au>
2017-07-08 15:01:52 +1000

    remove needless eslint config

--- machine-learning-with-javascript-part-1/.eslintrc.js
+++ machine-learning-with-javascript-part-1/.eslintrc.js
@@ -1,17 +1,5 @@
-// This is shared rules for all environments
 module.exports = {
   extends: [
     'airbnb-base',
-  ],
-  // parserOptions: {
-  //   sourceType: 'module'
-  // },
-  // env: {
-  //   jquery: true,
-  //   es6: true,
-  //   commonjs: true,
-  //   browser: true,
-  //   node: true,
-  //   'jest/globals': true
-  // }
+  ]
 };
