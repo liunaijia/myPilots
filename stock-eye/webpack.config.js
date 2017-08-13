b17e36f61edc0bf47d8abe24cdfec7c1df9adbf8
Naijia Liu <naijialiu@geniesolutions.com.au>
2017-08-13 17:56:12 +1000

    Add trade hours.

--- stock-eye/webpack.config.js
+++ stock-eye/webpack.config.js
@@ -9,4 +9,15 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
     filename: '[name].js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          { loader: 'babel-loader' },
+        ],
+      },
+    ],
+  },
 };
