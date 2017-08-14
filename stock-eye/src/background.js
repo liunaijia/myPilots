41cd47fc7f3d57a71c48a0931f46428b43aae903
Naijia Liu <naijialiu@geniesolutions.com.au>
2017-08-14 16:50:04 +1000

    Add badge to display the gap

--- stock-eye/src/background.js
+++ stock-eye/src/background.js
@@ -1,35 +1,29 @@
 import isTradeTime from './tradeTime';
 import { STOCK_POOL, THRESHOLD } from './settings';
 import fetchStockData from './stockData';
+import { setBadge, sendNotification } from './chromeApi';
 
 const process = (stocks = [{ buyingRatio: 0, sellingRatio: 0 }]) => {
   const stockMayBuy = stocks.sort((a, b) => a.sellingRatio - b.sellingRatio)[0];
   const stockMaySell = stocks.sort((a, b) => b.buyingRatio - a.buyingRatio)[0];
 
-  const delta = Math.round((stockMaySell.buyingRatio - stockMayBuy.sellingRatio) * 100) / 100;
-  if (delta > THRESHOLD) {
+  const gap = Math.round((stockMaySell.buyingRatio - stockMayBuy.sellingRatio) * 100) / 100;
+  if (gap > THRESHOLD) {
     return {
-      delta,
+      gap,
       buy: [stockMayBuy.code, stockMayBuy.sellingAt],
       sell: [stockMaySell.code, stockMaySell.buyingAt],
       timestamp: new Date(),
     };
   }
 
-  console.log(`${delta} B:${STOCK_POOL[stockMayBuy.code]}${stockMayBuy.sellingRatio}% S:${STOCK_POOL[stockMaySell.code]}${stockMaySell.buyingRatio}%`);
+  console.log(`${gap} B:${STOCK_POOL[stockMayBuy.code]}${stockMayBuy.sellingRatio}% S:${STOCK_POOL[stockMaySell.code]}${stockMaySell.buyingRatio}%`);
+  setBadge(gap.toString());
   return null;
 };
 
-const sendNotification = (title = '', message = '') => {
-  chrome.notifications.create({ type: 'basic', title, message, iconUrl: 'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7' });
-  // An easier way for notification is using Notification WebAPI:
-  // `new Notification(title, { body });`
-  // However the messages sent by Notification API come up with the website domain which is
-  // needless.
-};
-
-const sendDecision = ({ delta = 0, buy: [buyCode, buyPrice], sell: [sellCode, sellPrice] }) => {
-  const title = `价差${delta}%`;
+const sendDecision = ({ gap = 0, buy: [buyCode, buyPrice], sell: [sellCode, sellPrice] }) => {
+  const title = `价差${gap}%`;
   const message = `买${STOCK_POOL[buyCode]} ${buyPrice}，卖${STOCK_POOL[sellCode]} ${sellPrice}`;
   sendNotification(title, message);
 };
